c85506a977e0e559590a00788267079187b5969b
Bora Eryazgan <92815050+boraeryazgan@users.noreply.github.com>
2024-11-27 22:41:20 +0300

    User and Profile Section
    Initial process for MVP presentation

--- cseapp/app/controllers/application_controller.rb
+++ cseapp/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
+
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  helper_method :current_user
   allow_browser versions: :modern
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    unless current_user
+      redirect_to login_path, alert: "Giriş yapmanız gerekiyor."
+    end
+  end
 end
