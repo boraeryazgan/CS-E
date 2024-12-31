5515713de59c23f8508add8736883bac26898a3c
ozdemirrarda <ardaozdemir6767@hotmail.com>
2024-12-31 07:27:07 +0300

    a

--- cseapp/spec/rails_helper.rb
+++ cseapp/spec/rails_helper.rb
@@ -7,19 +7,11 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-# Uncomment the line below if you have `--require rails_helper` in the `.rspec` file
-# to avoid Rails generators crashing due to pending migrations
-# return unless Rails.env.test?
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support and its subdirectories.
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-require 'rspec/rails'
-
-# Additional requires below this line. Rails is loaded after this point.
-
-# Automatically require supporting ruby files with custom matchers and macros in spec/support/
-# It is recommended not to name files in spec/support with _spec.rb to avoid running specs twice.
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
-
-# Ensure migrations are applied before running tests
+# Checks for pending migrations and applies them before tests are run.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -27,14 +19,14 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-  # Fixture path, remove if you're not using fixtures
+  # If you're not using ActiveRecord or fixtures, you can remove the next line.
   config.fixture_path = Rails.root.join('spec/fixtures')
 
-  # Use transactional fixtures to wrap each test in a transaction
+  # If you're not using ActiveRecord, or you prefer not to run each test
+  # within a transaction, set this to false.
   config.use_transactional_fixtures = true
 
-  # Uncomment if you're not using ActiveRecord
-  # config.use_active_record = false
-
-  # Automatically infer test type based on location (e.g., /spec/models => type: :model)
-  config.infer
+  # Include the Rails helpers to test controllers, models, and views
+  config.include Rails.application.routes.url_helpers
+  config.include ActiveJob::TestHelper
+end
