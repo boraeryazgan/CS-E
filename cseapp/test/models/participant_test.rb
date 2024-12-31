a501a77ba424e0357cc8f3643509066100b416b3
ozdemirrarda <ardaozdemir6767@hotmail.com>
2024-12-31 06:05:16 +0300

    Update participant_test.rb

--- cseapp/test/models/participant_test.rb
+++ cseapp/test/models/participant_test.rb
@@ -1,7 +1,33 @@
-require "test_helper"
+require 'test_helper'
 
 class ParticipantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)    # `users(:one)` fixture verisini kullanıyoruz.
+    @room = rooms(:one)    # `rooms(:one)` fixture verisini kullanıyoruz.
+    @participant = Participant.new(user: @user, room: @room)
+  end
+
+  test "should be valid with valid user and room" do
+    assert @participant.valid?
+  end
+
+  test "should require a user" do
+    @participant.user = nil
+    assert_not @participant.valid?
+  end
+
+  test "should require a room" do
+    @participant.room = nil
+    assert_not @participant.valid?
+  end
+
+  test "should not allow duplicate user-room pair" do
+    # Önce bir katılımcı kaydediyoruz.
+    @participant.save
+    duplicate_participant = Participant.new(user: @user, room: @room)
+    
+    # Aynı kullanıcı ve oda kombinasyonuyla bir başka katılımcı kaydetmeye çalışıyoruz.
+    assert_not duplicate_participant.valid?
+    assert_includes duplicate_participant.errors[:user_id], "has already been taken"
+  end
 end
