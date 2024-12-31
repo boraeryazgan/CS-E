9c1482c4ad9214e6bdb3be0c35b439b9005f1b7a
Bora Eryazgan <92815050+boraeryazgan@users.noreply.github.com>
2024-12-31 09:41:14 +0300

    end
    Co-Authored-By: ozdemirrarda <168275163+ozdemirrarda@users.noreply.github.com>

--- cseapp/db/migrate/20241231051123_add_user_id_to_rooms.rb
+++
@@ -1,8 +0,0 @@
-class AddUserIdToRooms < ActiveRecord::Migration[7.0]
-  def change
-    
-    unless column_exists?(:rooms, :user_id)
-      add_column :rooms, :user_id, :integer
-    end
-  end
-end
