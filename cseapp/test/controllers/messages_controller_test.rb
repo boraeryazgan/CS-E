c403ad0e74dddc5a904cdf453c321e0eb5119547
ozdemirrarda <ardaozdemir6767@hotmail.com>
2024-12-31 05:54:28 +0300

    update

--- cseapp/test/controllers/messages_controller_test.rb
+++ cseapp/test/controllers/messages_controller_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one) 
+    @room = rooms(:one) 
+  end
+
+  test "should create message" do
+    sign_in @user 
+    message_body = "Hello, this is a test message!"
+
+    assert_difference('Message.count', 1) do
+      post room_messages_path(@room), params: { message: { body: message_body } }
+    end
+
+    assert_response :success
+    created_message = Message.last
+    assert_equal message_body, created_message.body
+    assert_equal @room.id, created_message.room_id
+    assert_equal @user.id, created_message.user_id
+  end
 end
--- cseapp/test/controllers/rooms_controller_test.rb
+++ cseapp/test/controllers/rooms_controller_test.rb
@@ -1,8 +1,29 @@
-require "test_helper"
+require 'test_helper'
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one) 
+    @room = rooms(:one) 
+
   test "should get index" do
-    get rooms_index_url
+    sign_in @user 
+    get rooms_url
     assert_response :success
+    assert_template 'index'
+  end
+
+  test "should show room" do
+    sign_in @user
+    get room_url(@room)
+    assert_response :success
+    assert_template 'index'
+  end
+
+  test "should create room" do
+    sign_in @user
+    assert_difference('Room.count', 1) do
+      post rooms_url, params: { room: { name: 'Test Room' } }
+    end
+    assert_response :redirect
   end
 end
