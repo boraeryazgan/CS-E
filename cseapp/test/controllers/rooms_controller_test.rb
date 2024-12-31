86e1797622a19cbd6482c922c582d8b02d08cea4
ozdemirrarda <ardaozdemir6767@hotmail.com>
2024-12-31 06:18:47 +0300

    Update rooms_controller_test.rb

--- cseapp/test/controllers/rooms_controller_test.rb
+++ cseapp/test/controllers/rooms_controller_test.rb
@@ -1,29 +1,30 @@
-require 'test_helper'
+require "test_helper"
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
+  # Test öncesi işlemler
   setup do
-    @user = users(:one) 
-    @room = rooms(:one) 
+    @user = users(:one) # Varsayılan bir kullanıcı kullanıyoruz
+    sign_in @user # Kullanıcıyı oturum açmış olarak işaretliyoruz
+  end
 
+  # "index" metodunun testi
   test "should get index" do
-    sign_in @user 
     get rooms_url
     assert_response :success
-    assert_template 'index'
   end
 
+  # "show" metodunun testi
   test "should show room" do
-    sign_in @user
-    get room_url(@room)
+    room = rooms(:one) # Test için bir oda alıyoruz
+    get room_url(room)
     assert_response :success
-    assert_template 'index'
   end
 
+  # "create" metodunun testi
   test "should create room" do
-    sign_in @user
-    assert_difference('Room.count', 1) do
-      post rooms_url, params: { room: { name: 'Test Room' } }
+    assert_difference('Room.count') do
+      post rooms_url, params: { room: { name: 'New Room' } }
     end
-    assert_response :redirect
+    assert_redirected_to room_url(Room.last)
   end
 end
