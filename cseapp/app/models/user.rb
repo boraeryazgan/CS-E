671af0e7a674d44848e28eea80af58537910ef32
ozdemirrarda <ardaozdemir6767@hotmail.com>
2024-12-31 08:14:03 +0300

    Update user.rb

--- cseapp/app/models/user.rb
+++ cseapp/app/models/user.rb
@@ -3,26 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
-  has_many :messages
+  
   # İlişkiler
   has_many :messages
+  has_many :rooms # Yeni ilişki: Bir kullanıcının birden fazla odası olabilir.
+  
+  # Arkadaşlık ilişkileri
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  has_many :friend_requests, foreign_key: :requestor_id, class_name: 'Friendship'
+  has_many :requested_friends, through: :friend_requests, source: :requestee
+
+  has_many :received_requests, foreign_key: :requestee_id, class_name: 'Friendship'
+  has_many :requestors, through: :received_requests, source: :requestor
 
   # Validasyonlar
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :gender, inclusion: { in: %w[male female other], message: "%{value} geçerli bir cinsiyet değil." }, allow_nil: true
-
-  has_many :friendships
-  has_many :friends, through: :friendships
-
-  
-  has_many :friend_requests, foreign_key: :requestor_id, class_name: 'Friendship'
-  has_many :requested_friends, through: :friend_requests, source: :requestee
-
-  
-  has_many :received_requests, foreign_key: :requestee_id, class_name: 'Friendship'
-  has_many :requestors, through: :received_requests, source: :requestor
 end
